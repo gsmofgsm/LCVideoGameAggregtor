7779475a489619dec8555c786ced5f6aeec64edd
Qing Ma <gsmofgsm@hotmail.com>
2020-12-20 15:53:38 +0100

    layouts.app

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <title>Laravel Video Games</title>
+</head>
+<body>
+
+</body>
+</html>
