c0caa61f153ab486438ca158ef648b18bcd1f372
Qing Ma <gsmofgsm@hotmail.com>
2020-12-21 15:48:40 +0100

    formatForView on PopularGames

--- app/Http/Livewire/PopularGames.php
+++ app/Http/Livewire/PopularGames.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class PopularGames extends Component
@@ -16,27 +17,43 @@ class PopularGames extends Component
         $before = Carbon::now()->subMonths(2)->timestamp;
         $after = Carbon::now()->addMonths(2)->timestamp;
 
-        $this->popularGames = Cache::remember('popular-games', 7, function() use ($before, $after) {
+        $popularGamesUnformatted = Cache::remember('popular-games', 7, function() use ($before, $after) {
             return Http::withHeaders([
                 'Client-ID' => config('services.igdb.id'),
                 'Authorization' => 'Bearer ' . config('services.igdb.token')
             ])->withBody(
                 "
-                fields name, cover.url, first_release_date, total_rating_count, platforms.abbreviation, rating,
+                fields name, cover.url, first_release_date, rating_count, platforms.abbreviation, rating,
                     slug;
                 where platforms = (48,49,130,6)
                 & (first_release_date >= {$before}
                 & first_release_date < {$after}
+                & rating_count > 5
                 );
-                sort total_rating_count desc;
+                sort rating_count desc;
                 limit 12;
                 ", 'text/plain'
             )->post('https://api.igdb.com/v4/games')->json();
         });
+
+//        dd($this->formatForView($popularGamesUnformatted));
+
+        $this->popularGames = $this->formatForView($popularGamesUnformatted);
     }
 
     public function render()
     {
         return view('livewire.popular-games');
+    }
+
+    private function formatForView($games)
+    {
+        return collect($games)->map(function ($game) {
+            return collect($game)->merge([
+                'coverImageUrl' => isset($game['cover']) ? Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) : '/ff7.jpg',
+                'rating' => isset($game['rating']) ? round($game['rating']) . '%' : null,
+                'platforms' => collect($game['platforms'])->pluck('abbreviation')->implode(', '),
+            ]);
+        });
     }
 }
