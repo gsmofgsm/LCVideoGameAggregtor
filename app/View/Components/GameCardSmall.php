a3813d21a8f760c47ddd1883d7425673d657fb45
Qing Ma <gsmofgsm@hotmail.com>
2020-12-21 16:36:39 +0100

    GameCardSmall

--- app/View/Components/GameCardSmall.php
+++ app/View/Components/GameCardSmall.php
@@ -6,14 +6,16 @@ use Illuminate\View\Component;
 
 class GameCardSmall extends Component
 {
+    public $game;
+
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($game)
     {
-        //
+        $this->game = $game;
     }
 
     /**
