6a946251ce27da4bf352aec1b3ff080bb609662b
Qing Ma <gsmofgsm@hotmail.com>
2020-12-21 11:32:21 +0100

    style fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,14 +30,14 @@
                     </div>
                 </div>
             @endforeach
-        </div>
+        </div> <!-- end popular-games -->
 
-        <div class="flex flex-col lg:flex:row my-10">
+        <div class="flex flex-col lg:flex-row my-10">
             <div class="recently-reviewed w-full lg:w-3/4 mr-0 lg:mr-32">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
                 <div class="recently-reviewed-container space-y-12 mt-8">
                     @foreach($recentlyReviewed as $game)
-                        <div class="game bg-gray-800 rounded-large shadow-md flex px-6 py-6">
+                        <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
                             <div class="relative flex-none">
                                 <a href="#">
                                     <img src="{{ isset($game['cover']) ? Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) : '/ff7.jpg' }}" alt="game cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
@@ -48,8 +48,8 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="ml-12">
-                                <a href="#" class="block text-large font-semibold leading-tight hover:text-gray-400 mt-4">
+                            <div class="ml-6 lg:ml-12">
+                                <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
                                     {{ $game['name'] }}
                                 </a>
                                 <div class="text-gray-400 mt-1">
@@ -63,7 +63,7 @@
                                     {{ $game['summary'] }}
                                 </p>
                             </div>
-                        </div>
+                        </div> <!-- end recentlyReviewed game -->
                     @endforeach
                 </div>
             </div>
@@ -84,9 +84,8 @@
                     </div>
                     @endforeach
                 </div>
-            </div>
-            <div class="most-anticipated lg:w-1/4 mt-12 lg:mt-0">
-                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Coming Soon</h2>
+
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold mt-12">Coming Soon</h2>
                 <div class="most-anticipated-container space-y-10 mt-8">
                     @foreach($comingSoon as $game)
                         <div class="game flex">
