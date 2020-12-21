6b9e645a1b81b2b5751a4aa7c2b1f24cd9cba255
Qing Ma <gsmofgsm@hotmail.com>
2020-12-21 12:15:20 +0100

    cache popular games

--- app/Http/Livewire/PopularGames.php
+++ app/Http/Livewire/PopularGames.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
@@ -15,11 +16,12 @@ class PopularGames extends Component
         $before = Carbon::now()->subMonths(2)->timestamp;
         $after = Carbon::now()->addMonths(2)->timestamp;
 
-        $this->popularGames = Http::withHeaders([
-            'Client-ID' => config('services.igdb.id'),
-            'Authorization' => 'Bearer ' . config('services.igdb.token')
-        ])->withBody(
-            "
+        $this->popularGames = Cache::remember('popular-games', 7, function() use ($before, $after) {
+            return Http::withHeaders([
+                'Client-ID' => config('services.igdb.id'),
+                'Authorization' => 'Bearer ' . config('services.igdb.token')
+            ])->withBody(
+                "
                 fields name, cover.url, first_release_date, total_rating_count, platforms.abbreviation, rating;
                 where platforms = (48,49,130,6)
                 & (first_release_date >= {$before}
@@ -28,7 +30,8 @@ class PopularGames extends Component
                 sort total_rating_count desc;
                 limit 12;
                 ", 'text/plain'
-        )->post('https://api.igdb.com/v4/games')->json();
+            )->post('https://api.igdb.com/v4/games')->json();
+        });
     }
 
     public function render()
