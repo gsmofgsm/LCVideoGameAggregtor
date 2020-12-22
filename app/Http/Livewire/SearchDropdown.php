8d12c68bc83d24901ec58a3f3ae53cc081618952
Qing Ma <gsmofgsm@hotmail.com>
2020-12-22 12:51:11 +0100

    search

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class SearchDropdown extends Component
 {
+    public $search = '';
+    public $searchResults = [];
+
     public function render()
     {
+        if (strlen($this->search) > 2) {
+            $this->searchResults = Http::withHeaders([
+                'Client-ID' => config('services.igdb.id'),
+                'Authorization' => 'Bearer ' . config('services.igdb.token')
+            ])->withBody(
+                "
+                search \"{$this->search}\";
+                fields name, slug, cover.url;
+                limit 6;
+                ", 'text/plain'
+            )->post('https://api.igdb.com/v4/games')->json();
+        }
         return view('livewire.search-dropdown');
     }
 }
