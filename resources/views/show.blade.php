2073e9f21bb73b6cc2a1b4765b1488bb5eb4c48e
Qing Ma <gsmofgsm@hotmail.com>
2020-12-22 13:49:26 +0100

    image modal div added

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -131,6 +131,25 @@
                 </div>
                 @endforeach
             </div>
+            <div
+                style="background-color: rgba(0, 0, 0, .5);"
+                class="z-50 fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+            >
+                <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                    <div class="bg-gray-900 rounded">
+                        <div class="flex justify-end pr-4 pt-2">
+                            <button
+                                class="text-3xl leading-none hover:text-gray-300"
+                            >
+                                &times;
+                            </button>
+                        </div>
+                        <div class="modal-body px-8 py-8">
+                            <img :src="image" alt="screenshot">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div> <!-- end images-container -->
 
         <div class="similar-games-container mt-8">
