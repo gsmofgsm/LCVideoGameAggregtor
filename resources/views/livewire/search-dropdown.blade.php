e88887be53a2cd4b8bb50b98741658e14184c4ef
Qing Ma <gsmofgsm@hotmail.com>
2020-12-22 13:16:28 +0100

    refs.search.focus

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -5,7 +5,15 @@
         @keydown="isVisible = true"
         @keydown.shift.tab="isVisible=false"
         type="text" class="bg-gray-800 text-sm rounded-full
-                    focus:outline-none focus:shadow-outline w-64 pl-8 px-3 py-1" placeholder="Search...">
+                    focus:outline-none focus:shadow-outline w-64 pl-8 px-3 py-1"
+        placeholder="Search (Press '/' to focus)"
+        x-ref="search"
+        @keydown.window="
+            if (event.keyCode === 191) {
+                event.preventDefault();
+                $refs.search.focus();
+            }
+        ">
     <div class="absolute top-0 flex items-center h-full ml-2">
         <svg class="fill-current text-gray-400 w-4" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
     </div>
